d578d1b1df4fd953c2f9147d2dd02ba97f06050c
leizingyiu <youcanfindme@126.com>
2023-10-13 03:26:07 +0800

    Create tagSleeper.user.js

---
+++ x_monkey_scripts/tagSleeper.user.js
@@ -0,0 +1,109 @@
+// ==UserScript==
+// @name        testing - 标签休眠/tagSleeper - 标签被切换到非活动状态时减少活动
+// @namespace   leizingyiu.net
+// @match       *://*/*
+// @grant       none
+// @version     2023/10/12 01:50:47
+// @author      leizingyiu
+// @description 2023/10/12 01:50:47
+// ==/UserScript==
+
+// Created: "2023/10/12 01:50:47"
+// Last modified: "2023/10/13 03:21:15" 
+
+
+//default_settings;
+let settings = {
+    video: {
+        autoPause: true,
+        autoRePlay: true
+    },
+
+    popAnAlert: true,
+    waitBeforeAlertJsSleep: 5,
+
+    tryStopCssAnimation: true
+};
+
+let website_settings = {
+    "bilibili.com": {
+        popAnAlert: false
+    }
+}
+
+let website = Object.keys(website_settings).filter(k => location.hostname.indexOf(k) != -1);
+website = website ? website[0] : false;
+if (website) {
+    settings = Object.assign(settings, website_settings[website]);
+}
+
+console.log(website, settings);
+
+const alertPauseClass = 'yiu_tagSleep_jsAlert',
+    videoClass = 'yiu_tagSleep_video_pause';
+
+
+function videoSleep(autoRePlay = settings.video.autoRePlay,
+    autoPause = settings.video.autoPause) {
+
+    if (document.hidden) {
+        if (autoPause) {
+            [...document.querySelectorAll('video')].map(video => {
+                if (!video.paused) {
+                    video.classList.add(videoClass);
+                    video.pause();
+                };
+            })
+        }
+    } else {
+        if (document.documentElement.classList.contains(alertPauseClass)) {
+            return false;
+        }
+        if (autoRePlay) {
+            [...document.querySelectorAll('video')].map(video => {
+                if (video.classList.contains(videoClass)) {
+                    video.classList.remove(videoClass);
+                    video.play();
+                };
+            });
+        }
+    }
+}
+
+alertSleeper = null;
+document.documentElement.classList.remove(alertPauseClass);
+function alertToPauseJs(boo = settings.popAnAlert, waitTime = settings.waitBeforeAlertJsSleep * 1000) {
+    console.log(boo);
+    if (!Boolean(boo)) { return; }
+
+    let oldSleeper = Boolean(alertSleeper);
+    if (document.hidden && Boolean(!alertSleeper)) {
+        alertSleeper = setTimeout(function () {
+            document.documentElement.classList.toggle(alertPauseClass);
+            let _boo = confirm('休眠中，点击确定继续播放/浏览⬇️');
+            if (_boo) { videoSleep(); }
+
+        }, waitTime);
+    } else {
+        clearTimeout(alertSleeper);
+        alertSleeper = null;
+    }
+
+    if (oldSleeper != Boolean(alertSleeper)) {
+        document.documentElement.classList.toggle(alertPauseClass);
+        videoSleep();
+    }
+}
+
+
+document.addEventListener("visibilitychange", function () {
+
+
+    videoSleep();
+
+    alertToPauseJs();
+
+});
+
+
+
